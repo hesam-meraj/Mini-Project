a8e212d7167feae852e6de2db57533309d2ca97b
hesam-meraji <mh.meraji12345@gmail.com>
2024-09-01 17:33:18 +0330

    building first version of data model

--- books_management/models.py
+++ books_management/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    publish_at = models.DateField()
+    number_of_pages = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+
+class Author(models.Model):
+    GENDER_CHOICES = [
+        ("M", 'Male'),
+        ("F", 'Female'),
+        ("O", 'Other')
+    ]
+
+    name = models.CharField(max_length=255)
+    birth_date = models.DateField()
+    gender  = models.CharField(max_length=1,choices=GENDER_CHOICES,default='M')
+    
+
+
+class Ownership(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
