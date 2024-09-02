95c5fd7f2166518217dd1b629b509af6b7bda0de
hesam-meraji <mh.meraji12345@gmail.com>
2024-09-02 04:39:35 +0330

    starting admin pannel

--- books_management/models.py
+++ books_management/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    def __str__(self) -> str:
+        return self.name
 
 
 class Book(models.Model):
@@ -10,7 +12,9 @@ class Book(models.Model):
     description = models.TextField()
     published_at = models.DateField()
     number_of_pages = models.PositiveIntegerField()
-    category = models.ForeignKey(Category, on_delete=models.PROTECT,null=True)
+    # category = models.ForeignKey(Category, on_delete=models.PROTECT,null=True)
+    def __str__(self) -> str:
+        return self.name
 
 
 class Author(models.Model):
@@ -23,9 +27,12 @@ class Author(models.Model):
     name = models.CharField(max_length=255)
     birth_date = models.DateField()
     gender  = models.CharField(max_length=1,choices=GENDER_CHOICES,default='M')
-    
+    def __str__(self) -> str:
+        return self.name
 
 
 class Ownership(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.name
